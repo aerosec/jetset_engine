b34fcd82c699b404fcd04be941fd7784848b735c
enjhnsn2 <e5johnso@eng.ucsd.edu>
2021-06-06 20:37:32 -0500

    unncessary comments

--- jetset_engine/scripts/eval.py
+++ jetset_engine/scripts/eval.py
@@ -4,10 +4,6 @@ def base_stats(dev):
   print("num reads", len([v for v in dev.trace if (v.is_ioport or v.is_mmio) and v.is_read]))
   print("num distinct write addrs: ", len(set([(v.addr if v.is_mmio else v.portno) for v in dev.trace if (v.is_ioport or v.is_mmio) and not v.is_read])))
   print("num distinct read addrs: ", len(set([(v.addr if v.is_mmio else v.portno) for v in dev.trace if (v.is_ioport or v.is_mmio) and v.is_read])))
-  # start = datetime.now()
-  # dev.mk_device()
-  # end = datetime.now()
-  # print("Synthesis Time: ", end - start)
 
 def constraint_stats(dev):
   import statistics
@@ -17,12 +13,9 @@ def constraint_stats(dev):
   for v in vars:
     s = list(v.symbol.variables)[0]
     sums.append(sum([len(c[1]) for c in ics if s in c[0]]))
-  # print("num constraints", sum(sums))
   print("average constraints per variable:",statistics.mean(sums))
-  # print(max(sums))
 
 def count_trace_lens(dev):
-  # MMIO first
   from collections import Counter
   import statistics
   v1 = list(Counter([v.addr for v in dev.trace if (v.is_mmio) and v.is_read]).values())
@@ -32,17 +25,3 @@ def count_trace_lens(dev):
   print(statistics.median(tot))
   print(max(tot))
 
-# IDA block counter
-'''
-from idautils import *
-from idaapi import *
-from idc import *
-blockCount = 0
-for segea in Segments():
-  for funcea in Functions(segea, SegEnd(segea)):
-    f = get_func(funcea)
-    fc = FlowChart(f)
-    for b in fc:
-      blockCount += 1
-print(blockCount)
-'''
