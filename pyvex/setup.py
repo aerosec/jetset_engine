b9930762118b4a2e62ace95097b014a4d536bf52
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 19:08:47 +0000

    Adding numeric_owner as keyword arguement

--- pyvex/setup.py
+++ pyvex/setup.py
@@ -84,7 +84,7 @@ if not os.path.exists(VEX_PATH):
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(tar)
